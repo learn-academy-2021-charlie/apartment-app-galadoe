0c8c32f048d6cbcdf4d2eacf1bfffbd5a3ef78d5
galadoe <kgaladoe@gmail.com>
2021-08-31 10:12:14 -0700

    added bootstap, reactstrap, and minor fixes

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,6 +1,6 @@
 import React from "react"
 import PropTypes from "prop-types"
-import { BrowserRouter as  Router, Route, Switch } from "react-router-dom"
+import { BrowserRouter as  Router, Route, Switch, NavLink } from "react-router-dom"
 import { Nav, NavItem } from "reactstrap"
 
 import AboutUs from "./pages/AboutUs"
