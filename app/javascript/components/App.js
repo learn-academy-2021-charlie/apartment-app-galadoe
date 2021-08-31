ff6afbac0c040b001da46cb0878bf691aaca5776
galadoe <kgaladoe@gmail.com>
2021-08-31 09:41:24 -0700

    rail routes, added pages, react router, basic navigation

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,9 +1,36 @@
 import React from "react"
 import PropTypes from "prop-types"
+import { BrowserRouter as  Router, Route, Switch } from "react-router-dom"
+import { Nav, NavItem } from "reactstrap"
+
+import AboutUs from "./pages/AboutUs"
+import LearnMore from "./pages/LearnMore"
+import Home from "./pages/Home"
+
 class App extends React.Component {
   render () {
     return (
       <React.Fragment>
+        <h1>Hello World</h1>
+        <Router>
+          <Nav>
+            <NavItem>
+              <NavLink to="/">Home</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink to="/about">About Us</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink to="/learn">Learn More</NavLink>
+            </NavItem>
+          </Nav>
+          <Switch>
+            <Route exact path="/" component={ Home } />
+            <Route path="/about" component={ AboutUs } />
+            <Route path="/learn" component={ LearnMore } />
+          </Switch>
+        </Router>
+
       </React.Fragment>
     );
   }
