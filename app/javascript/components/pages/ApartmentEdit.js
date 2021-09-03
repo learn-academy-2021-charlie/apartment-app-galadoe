839893f5548624febbbd67c1240982adfda9f86e
galadoe <kgaladoe@gmail.com>
2021-09-02 21:19:18 -0700

    can click on apartment and edit it

--- app/javascript/components/pages/ApartmentEdit.js
+++ app/javascript/components/pages/ApartmentEdit.js
@@ -46,7 +46,7 @@ class ApartmentEdit extends Component {
     return(
       <>
         <div>
-          <h3>Edit Apartment information</h3>
+          <h3>Edit {this.props.apartment.street}'s information</h3>
           <Form>
             <FormGroup>
               <Label for="street">Street</Label>
@@ -129,7 +129,7 @@ class ApartmentEdit extends Component {
           </button>
         </div>
 
-        {this.state.submitted && <Redirect to={`/apartmentshow/${this.state.form}`} /> }
+        {this.state.submitted && <Redirect to={`/apartmentshow/${this.state.form.id}`} /> }
       </>
       )
     }
--- app/javascript/components/pages/ApartmentShow.js
+++ app/javascript/components/pages/ApartmentShow.js
@@ -1,8 +1,23 @@
 import React, {Component} from "react"
 import { Container, Row, Col, Card, CardTitle, CardBody, CardText, Button, CardSubtitle} from 'reactstrap'
 import mockApartment from '../mockApartment'
+import { NavLink, Redirect} from "react-router-dom"
 
 class ApartmentShow extends Component {
+  constructor(props){
+    super(props)
+
+    this.state = {
+      submitted: false
+    }
+  }
+
+  handleSubmit = (e) => {
+    const { apartment } = this.props
+    this.props.deleteApartment(apartment.id)
+    this.setState({ submitted: true })
+  }
+
   render(){
     const {apartment} = this.props
     return(
@@ -20,8 +35,15 @@ class ApartmentShow extends Component {
               { `${apartment && apartment.bedrooms} bedrooms and ${apartment && apartment.bathrooms} baths. Monthly Rent: ${apartment && apartment.price}` }
             </CardSubtitle>
           </Card>
-        </div>
+          <Button className="button-style">
+            <NavLink to={`/apartmentedit/${apartment.id}`} id="edit-nav">Edit Apartment</NavLink>
+          </Button>
+          <Button className="button-style" onClick={this.handleSubmit}>
+            Delete Apartment
+          </Button>
 
+        </div>
+            { this.state.submitted && <Redirect to={`/apartmentindex/`} />}
       </>
     )
   }
